8e611b84ef71daef22b3249a3d9710c03f7ba0a2
Tian Li <tianli.tl@alibaba-inc.com>
2023-08-25 14:06:39 +0800

    fix bug

--- src/argparse.cpp
+++ src/argparse.cpp
@@ -1,4 +1,4 @@
-include "argparse.h"
+#include "argparse.h"
 
 void get_opts(int argc,
               char **argv,
@@ -7,45 +7,55 @@ void get_opts(int argc,
     if (argc == 1)
     {
         std::cout << "Usage:" << std::endl;
-        std::cout << "\t--in or -i <file_path>" << std::endl;
-        std::cout << "\t--out or -o <file_path>" << std::endl;
-        std::cout << "\t--n_threads or -n <num_threads>" << std::endl;
-        std::cout << "\t--loops or -l <num_loops>" << std::endl;
-        std::cout << "\t[Optional] --spin or -s" << std::endl;
+        std::cout << "\t--num_cluster or -k <num_cluster>" << std::endl;
+        std::cout << "\t--dims or -d <dims>" << std::endl;
+        std::cout << "\t--inputfilename or -i <inputfilename>" << std::endl;
+        std::cout << "\t--max_num_iter or -m <max_num_iter>" << std::endl;
+        std::cout << "\t--threshold or -t <threshold>" << std::endl;
+        std::cout << "\t--seed or -s <seed>" << std::endl;
+        std::cout << "\t[Optional] --centroids or -c" << std::endl;
         exit(0);
     }
 
-    opts->spin = false;
+    opts->centroids = false;
 
     struct option l_opts[] = {
-        {"in", required_argument, NULL, 'i'},
-        {"out", required_argument, NULL, 'o'},
-        {"n_threads", required_argument, NULL, 'n'},
-        {"loops", required_argument, NULL, 'l'},
-        {"spin", no_argument, NULL, 's'}
+        {"num_cluster", required_argument, NULL, 'k'},
+        {"dims", required_argument, NULL, 'd'},
+        {"inputfilename", required_argument, NULL, 'i'},
+        {"max_num_iter", required_argument, NULL, 'm'},
+        {"threshold", required_argument, NULL, 't'},
+        {"seed", required_argument, NULL, 's'},
+        {"centroids", no_argument, NULL, 'c'}
     };
 
     int ind, c;
-    while ((c = getopt_long(argc, argv, "i:o:n:sl:", l_opts, &ind)) != -1)
+    while ((c = getopt_long(argc, argv, "k:d:i:m:t:cs:", l_opts, &ind)) != -1)
     {
         switch (c)
         {
         case 0:
             break;
+        case 'k':
+            opts->num_cluster = atoi((char *)optarg);
+            break;
+        case 'd':
+            opts->dims = atoi((char *)optarg);
+            break;
         case 'i':
-            opts->in_file = (char *)optarg;
+            opts->inputfilename = (char *)optarg;
             break;
-        case 'o':
-            opts->out_file = (char *)optarg;
+        case 'm':
+            opts->max_num_iter = atoi((char *)optarg);
             break;
-        case 'n':
-            opts->n_threads = atoi((char *)optarg);
+        case 't':
+            opts->threshold = atof((char *)optarg);
+            break;
+        case 'c':
+            opts->centroids = true;
             break;
         case 's':
-            opts->spin = true;
-            break;
-        case 'l':
-            opts->n_loops = atoi((char *)optarg);
+            opts->seed = atoi((char *)optarg);
             break;
         case ':':
             std::cerr << argv[0] << ": option -" << (char)optopt << "requires an argument." << std::endl;
